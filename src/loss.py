e1096205f8fa6ac14ac84a83badd5b84a5dd4218
SungChul Hong <chulhongsung@gmail.com>
2021-05-15 20:42:08 +0900

    add loss.py train.py

---
+++ src/loss.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow import keras as K
+
+class SoftNcut_loss(K.losses.Loss):
+    def __init__(self, K, N):
+        super(SoftNcut_loss, self).__init__()
+        self.K = K # num class
+        self.N = N # image row * col
+    
+    def call(self, weights, img_seg):
+        batch_size = tf.cast(img_seg.shape[0], dtype=tf.int32)
+        weights_ = tf.transpose(weights[tf.newaxis, ...], [1, 0, 2, 3]) 
+        flat_img_seg= tf.reshape(img_seg, [batch_size, self.N, 1, self.K])
+        prob_product = tf.linalg.matmul(tf.transpose(flat_img_seg, [0, 3, 1, 2]), tf.transpose(flat_img_seg, [0, 3, 2, 1]))
+        
+        assoc_nomi = tf.math.reduce_sum(weights_ *  prob_product)
+        assoc_denomi = tf.math.reduce_sum(tf.transpose(flat_img_seg, [0, 3, 1, 2]) * weights_)
+        
+        softncut_loss = self.K - assoc_nomi / assoc_denomi
+        
+        return softncut_loss
+#%%
+class Recon_loss(K.losses.Loss):
+    def __init__(self):
+        super(Recon_loss, self).__init__()
+        
+    def call(self, truth, recon):
+        recon_loss = tf.math.reduce_sum(K.losses.MSE(truth, recon))
+        
+        return recon_loss
---
+++ src/train.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow import keras as K
+import numpy as np
+
+weights = np.load("ncut_weights_stl10_30.npy")
+train_img = np.load("train_stl10_30.npy")
+
+BATCH_SIZE = 5
+stl10_30 = tf.data.Dataset.from_tensor_slices((np.array(weights, dtype=np.float32), train_img))
+batch_stl10_30 = stl10_30.batch(BATCH_SIZE)
+
+optimizer = K.optimizers.Adam(learning_rate=0.001)
+train_loss = K.metrics.Mean(name='train_loss')
+
+@tf.function
+def train_step(model, weights, image):
+    with tf.GradientTape(persistent=True) as tape:
+        img_seg, recon = model(image)
+        sn_loss = softncut_loss(weights, img_seg)
+        rc_loss = recon_loss(image, recon)
+        loss = sn_loss + rc_loss
+    grad1 = tape.gradient(sn_loss, model.unet_encoder.trainable_weights)
+    grad2 = tape.gradient(rc_loss, model.trainable_weights)
+    optimizer.apply_gradients(zip(grad1, model.unet_encoder.trainable_weights))
+    optimizer.apply_gradients(zip(grad2, model.trainable_weights))
+    train_loss(loss)
+    
+EPOCHS = 10
+
+for epoch in range(EPOCHS):
+    for batch_weights, batch_image in iter(batch_stl10_30):
+        train_step(wnet, batch_weights, batch_image)
+    template = 'EPOCH: {}, Train Loss: {}'
+    print(template.format(epoch+1, train_loss.result()))
