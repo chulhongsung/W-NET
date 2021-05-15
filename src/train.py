2d5eb365914aa8ad72e8ad423d022339789b42c9
SungChul Hong <chulhongsung@gmail.com>
2021-05-15 22:02:25 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 from tensorflow import keras as K
 import numpy as np
 
+from loss import SoftNcut_loss, Recon_loss
+from wnet import Wnet
+
 weights = np.load("ncut_weights_stl10_30.npy")
 train_img = np.load("train_stl10_30.npy")
 
@@ -9,7 +12,7 @@ BATCH_SIZE = 5
 stl10_30 = tf.data.Dataset.from_tensor_slices((np.array(weights, dtype=np.float32), train_img))
 batch_stl10_30 = stl10_30.batch(BATCH_SIZE)
 
-optimizer = K.optimizers.Adam(learning_rate=0.001)
+optimizer = K.optimizers.Adam(learning_rate=0.01)
 train_loss = K.metrics.Mean(name='train_loss')
 
 @tf.function
@@ -24,8 +27,10 @@ def train_step(model, weights, image):
     optimizer.apply_gradients(zip(grad1, model.unet_encoder.trainable_weights))
     optimizer.apply_gradients(zip(grad2, model.trainable_weights))
     train_loss(loss)
-    
-EPOCHS = 10
+
+wnet = Wnet(10)    
+
+EPOCHS = 100
 
 for epoch in range(EPOCHS):
     for batch_weights, batch_image in iter(batch_stl10_30):
