7ec4238a8d4afa1516d065ab6bd0958b77c9ee15
SungChul Hong <chulhongsung@gmail.com>
2021-05-17 13:32:56 +0900

    update loss.py

--- src/loss.py
+++ src/loss.py
@@ -2,21 +2,21 @@ import tensorflow as tf
 from tensorflow import keras as K
 
 class SoftNcut_loss(K.losses.Loss):
-    def __init__(self, K, N):
+    def __init__(self, C, N):
         super(SoftNcut_loss, self).__init__()
-        self.K = K # num class
+        self.C = C # num class
         self.N = N # image row * col
     
     def call(self, weights, img_seg):
         batch_size = tf.cast(img_seg.shape[0], dtype=tf.int32)
         weights_ = tf.transpose(weights[tf.newaxis, ...], [1, 0, 2, 3]) 
-        flat_img_seg= tf.reshape(img_seg, [batch_size, self.N, 1, self.K])
+        flat_img_seg= tf.reshape(img_seg, [batch_size, self.N, 1, self.C])
         prob_product = tf.linalg.matmul(tf.transpose(flat_img_seg, [0, 3, 1, 2]), tf.transpose(flat_img_seg, [0, 3, 2, 1]))
         
         assoc_nomi = tf.math.reduce_sum(weights_ *  prob_product)
         assoc_denomi = tf.math.reduce_sum(tf.transpose(flat_img_seg, [0, 3, 1, 2]) * weights_)
         
-        softncut_loss = self.K - assoc_nomi / assoc_denomi
+        softncut_loss = self.C - assoc_nomi / assoc_denomi
         
         return softncut_loss
 
