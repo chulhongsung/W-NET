2c1196173dd952f77b8f17f605ec5bb70115fc0e
SungChul Hong <chulhongsung@gmail.com>
2021-05-24 13:40:27 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ from loss import SoftNcut_loss, Recon_loss
 from wnet import Wnet
 from crf import crf_batch_fit_predict
 
-weights = np.load("ncut_weights_stl10_30.npy")
+# weights = np.load("ncut_weights_stl10_30.npy")
 # train_img = np.load("train_stl10_30.npy")
 
 train_stl10, test_stl10 = tfds.load('stl10', split=['train', 'test'], shuffle_files=True)
@@ -33,6 +33,42 @@ label = []
 for i in range(num_image):
     label.append(train_img_objects[i].numpy().tolist())
 
+image = train_img.numpy()
+
+weight_size = np.prod(image.shape[1:3])
+batch_size = image.shape[0]
+bright_weights = np.zeros((batch_size,weight_size,weight_size))
+reduce_image = np.mean(image,axis=3)
+
+sigma_X = 4
+sigma_I = 100
+r = 5
+
+for batch in range(batch_size):
+    # Reduce channel
+    flat_image = np.ravel(reduce_image[batch])
+    
+    # Gaussian neighbor
+    Fj, Fi = np.meshgrid(flat_image, flat_image)
+    X, Y = list(zip(*np.ndindex(image.shape[1:3])))
+    Xj, Xi = np.meshgrid(X,X)
+    Yj, Yi = np.meshgrid(Y,Y)
+    X_metric = np.sqrt((Xi - Xj)**2 + (Yi - Yj)**2)
+    F_metric = np.abs(Fi - Fj)
+    
+    # Brightness weight
+    bright_weight = np.exp(-(X_metric**2 / sigma_X**2) -(F_metric**2 / sigma_I**2))
+    bright_weight[X_metric >= r] = 0
+    bright_weights[batch] = bright_weight
+
+# np.save("ncut_weights_stl10.npy", bright_weights)
+
+# bright_weights = bright_weights[0:30]
+# train_img = train_img[0:30]
+
+# np.save("ncut_weights_stl10_30.npy", bright_weights)
+# np.save("train_stl10_30.npy", train_img)
+    
 BATCH_SIZE = 5
 NUM_CLASS = 5
 
